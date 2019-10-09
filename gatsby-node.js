780413d85631f6e9700ec85a19bf94d445584685
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-10 01:25:16 +0200

    improve gatsby-node mechanism

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,2 +1,50 @@
-exports.createPages = require('./gatsby/createPages')
-exports.onCreateNode = require('./gatsby/onCreateNode')
+const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require("path")
+
+const replacePath = path => (path === `/` ? path : path.replace(/\/$/, ``))
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const postTemplate = path.resolve(`src/templates/postTemplate.tsx`)
+
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: replacePath(node.fields.slug),
+        component: postTemplate,
+        context: {}, // additional data can be passed via context
+      })
+    })
+  })
+}
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: replacePath(slug),
+    })
+  }
+}
