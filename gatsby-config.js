f68728bcc754c0ab3695b429901511c3451b0250
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-10 10:59:38 +0200

    add node-sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,11 @@
+const path = require(`path`)
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Not everyone can be Gandhi`,
+    description: `Not everyone can be Gandhi, but each of us has the power to make sure our own lives count – and it’s those millions of lives that will ultimately build a better world. – Jeffrey Skoll`,
+    author: `Pierre-Alexis Blond`,
+    authorDescription: "try to make the world better",
   },
   plugins: [
     {
@@ -31,10 +34,31 @@ module.exports = {
         icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 620,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src/assets/images`),
+      },
+    },
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-typescript`,
+    `gatsby-plugin-sass`,
   ],
 }
